5e6a37fcc23213d4cfa6d10b21c30bb6bc6d3f3e
AryaEm <ary4maulanaa@gmail.com>
2026-01-10 14:59:16 +0700

    fix error

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,9 +1,13 @@
 import AuthContainer from '@/components/auth/auth-container';
 
-export default function AuthPage({
-  searchParams,
-}: {
-  searchParams: { mode?: string };
-}) {
-  return <AuthContainer mode={searchParams.mode ?? 'login'} />;
+type AuthPageProps = {
+  searchParams: Promise<{
+    mode?: string;
+  }>;
+};
+
+export default async function AuthPage({ searchParams }: AuthPageProps) {
+  const params = await searchParams;
+
+  return <AuthContainer mode={params.mode ?? 'login'} />;
 }
